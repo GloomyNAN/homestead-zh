ab6d4cfef055c9752ec6a7ae8e115a7896095fbd
Gloomy楠 <GloomyNAN@163.com>
2019-01-28 15:42:14 +0800

    first commit

---
+++ after.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# If you would like to do some extra provisioning you may
+# add any commands you wish to this file and they will
+# be run after the Homestead machine is provisioned.
+sudo apt-get update
+sudo apt-get install php7.1-mcrypt php7.1-gmp php7.3-gmp  php-redis
