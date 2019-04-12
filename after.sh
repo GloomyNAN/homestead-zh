1300fc4fe166b98e3e177b637cc215aef1acae90
Gloomy楠 <GloomyNAN@163.com>
2019-04-12 11:00:40 +0800

    增加插件

--- after.sh
+++ after.sh
@@ -4,4 +4,6 @@
 # add any commands you wish to this file and they will
 # be run after the Homestead machine is provisioned.
 sudo apt-get update
-sudo apt-get install php7.1-mcrypt php7.1-gmp php7.3-gmp  php-redis
+
+# wkhtmltopdf H5生成pdf插件，教师简历生成用
+sudo apt-get install php7.1-mcrypt php7.1-gmp php7.3-gmp  php-redis wkhtmltopdf -y
