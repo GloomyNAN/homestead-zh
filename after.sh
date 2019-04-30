95af926c32b3ef4dcc7f438f2ef0d192f22f08b6
Gloomy楠 <GloomyNAN@163.com>
2019-04-30 13:18:32 +0800

    php-ext

--- after.sh
+++ after.sh
@@ -5,5 +5,23 @@
 # be run after the Homestead machine is provisioned.
 sudo apt-get update
 
+# 修改composer镜像
+composer config -g repo.packagist composer https://packagist.phpcomposer.com
+
+# npm还原
+sudo npm config set registry https://registry.npm.taobao.org
+
+# php扩展
+sudo apt-get install php-redis -y
+
+## php7.1扩展
+sudo apt-get install php7.1-mcrypt php7.1-gmp php7.1-simplexml -y
+
+## php7.2扩展
+sudo apt-get install php7.2-gmp php7.2-mbstring php7.2-xml -y 
+
+## php7.3扩展
+sudo apt-get install php7.3-gmp -t
+
 # wkhtmltopdf H5生成pdf插件，教师简历生成用
-sudo apt-get install php7.1-mcrypt php7.1-gmp php7.3-gmp  php-redis wkhtmltopdf -y
+wkhtmltopdf -y
